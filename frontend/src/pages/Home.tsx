793603d0bee3cb07dff7d3897ec1af8b2d933053
Arthur Hennig <ahennig@student-net-hg-0777.intern.ethz.ch>
2024-10-11 21:24:23 +0200

    Mensa Component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,21 +5,37 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "./Home.css";
 import Header from "../components/Header";
+import MensaCard from "../components/MensaCard";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <Header />
       <IonContent fullscreen>
+        <Swiper slidesPerView={3} direction={"horizontal"}>
+          <SwiperSlide>
+            <MensaCard
+              name="Polymensa"
+              open={true}
+              image={"public/polymensa.jpg"}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <MensaCard
+              name="Archimedes"
+              open={false}
+              image={"public/archimedes.jpg"}
+            />
+          </SwiperSlide>
+        </Swiper>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Home</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Home page" />
       </IonContent>
     </IonPage>
   );
