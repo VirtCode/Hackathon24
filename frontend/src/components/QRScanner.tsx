2b5771920f1d6703675758f6449756366152de5c
noex0905 <noelschaller23@gmail.com>
2024-10-12 23:31:56 +0200

    Qr code logic

--- frontend/src/components/QRScanner.tsx
+++ frontend/src/components/QRScanner.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 
 // Styles
 import "./QrStyles.css";
@@ -6,27 +6,39 @@ import "./QrStyles.css";
 // Qr Scanner
 import QrScanner from "qr-scanner";
 import QrFrame from "../assets/qr-frame.svg";
-import { IonButton } from "@ionic/react";
+import { IonButton, IonItem, useIonRouter } from "@ionic/react";
 
-const QrReader = () => {
+interface QrScannerProps {
+  setModalIsOpen: Dispatch<SetStateAction<boolean>>;
+  setModelTitle: Dispatch<SetStateAction<string>>;
+}
+
+const QrReader: React.FC<QrScannerProps> = ({
+  setModalIsOpen,
+  setModelTitle,
+}) => {
+  const router = useIonRouter();
+
   // QR States
   const scanner = useRef<QrScanner>();
   const videoEl = useRef<HTMLVideoElement>(null);
   const qrBoxEl = useRef<HTMLDivElement>(null);
-  const [qrOn, setQrOn] = useState<boolean>(false);
+  const [qrOn, setQrOn] = useState<boolean>(true);
 
   // Result
   const [scannedResult, setScannedResult] = useState<string | undefined>("");
+  const [addToSessionMode, setAddToSessionMode] = useState<boolean>(false);
+  const [newSessionMode, setNewSessionMode] = useState<boolean>(false);
 
   // Success
   const onScanSuccess = (result: QrScanner.ScanResult) => {
-    // 🖨 Print the "result" to browser console.
     console.log(result);
-    // ✅ Handle success.
-    // 😎 You can do whatever you want with the scanned result.
     setScannedResult(result?.data);
-    scanner?.current?.stop();
-    setQrOn(false);
+    scanner.current?.stop();
+    setModelTitle("Select next Step");
+    // router.push(result.data, "root", "replace")
+    // setModalIsOpen(false);
+    // setQrOn(false);
   };
 
   // Fail
@@ -35,85 +47,82 @@ const QrReader = () => {
     console.log(err);
   };
 
-  function startScanner() {
-    setQrOn(true);
+  useEffect(() => {
     if (videoEl?.current && !scanner.current) {
-        // 👉 Instantiate the QR Scanner
-        scanner.current = new QrScanner(videoEl?.current, onScanSuccess, {
-          onDecodeError: onScanFail,
-          // 📷 This is the camera facing mode. In mobile devices, "environment" means back camera and "user" means front camera.
-          preferredCamera: "environment",
-          // 🖼 This will help us position our "QrFrame.svg" so that user can only scan when qr code is put in between our QrFrame.svg.
-          highlightScanRegion: true,
-          // 🔥 This will produce a yellow (default color) outline around the qr code that we scan, showing a proof that our qr-scanner is scanning that qr code.
-          highlightCodeOutline: true,
-          // 📦 A custom div which will pair with "highlightScanRegion" option above 👆. This gives us full control over our scan region.
-          overlay: qrBoxEl?.current || undefined,
-        });
-  
-        // 🚀 Start QR Scanner
-        scanner?.current
-          ?.start()
-          .then(() => setQrOn(true))
-          .catch((err) => {
-            if (err) setQrOn(false);
-          });
-      }
-  
-      // 🧹 Clean up on unmount.
-      // 🚨 This removes the QR Scanner from rendering and using camera when it is closed or removed from the UI.
-    //   return () => {
-    //     if (!videoEl?.current) {
-    //       scanner?.current?.stop();
-    //     }
-    //   };
-  }
+      setModelTitle("Scan QR Code");
+      // 👉 Instantiate the QR Scanner
+      scanner.current = new QrScanner(videoEl?.current, onScanSuccess, {
+        onDecodeError: onScanFail,
+        // 📷 This is the camera facing mode. In mobile devices, "environment" means back camera and "user" means front camera.
+        preferredCamera: "environment",
+        // 🖼 This will help us position our "QrFrame.svg" so that user can only scan when qr code is put in between our QrFrame.svg.
+        highlightScanRegion: true,
+        // 🔥 This will produce a yellow (default color) outline around the qr code that we scan, showing a proof that our qr-scanner is scanning that qr code.
+        highlightCodeOutline: true,
+        // 📦 A custom div which will pair with "highlightScanRegion" option above 👆. This gives us full control over our scan region.
+        overlay: qrBoxEl?.current || undefined,
+      });
 
-//   useEffect(() => {
-    
-//   }, []);
+      // 🚀 Start QR Scanner
+      scanner?.current
+        ?.start()
+        .then(() => setQrOn(true))
+        .catch((err) => {
+          if (err) setQrOn(false);
+        });
+    }
+
+    // 🧹 Clean up on unmount.
+    // 🚨 This removes the QR Scanner from rendering and using camera when it is closed or removed from the UI.
+    return () => {
+      if (!videoEl?.current) {
+        scanner?.current?.stop();
+      }
+    };
+  }, []);
 
   // ❌ If "camera" is not allowed in browser permissions, show an alert.
-//   useEffect(() => {
-//     if (!qrOn)
-//       alert(
-//         "Camera is blocked or not accessible. Please allow camera in your browser permissions and Reload."
-//       );
-//   }, [qrOn]);
+  useEffect(() => {
+    if (!qrOn)
+      alert(
+        "Camera is blocked or not accessible. Please allow camera in your browser permissions and Reload."
+      );
+  }, [qrOn]);
 
   return (
     <>
-    {qrOn? (<div className="qr-reader">
-          {/* QR */}
-          <video ref={videoEl}></video>
-          <div ref={qrBoxEl} className="qr-box">
+      {!scannedResult ? (
+        <>
+          <div className="qr-reader">
+            {/* QR */}
+            <video ref={videoEl}></video>
+            <div ref={qrBoxEl} className="qr-box">
               <img
-                  src={QrFrame}
-                  alt="Qr Frame"
-                  width={256}
-                  height={256}
-                  className="qr-frame" />
+                src={QrFrame}
+                alt="Qr Frame"
+                width={256}
+                height={256}
+                className="qr-frame"
+              />
+            </div>
           </div>
-
-          {/* Show Data Result if scan is success */}
-          
-      </div>): null}
-    {scannedResult && (
-              <p
-                  style={{
-
-                      zIndex: 99999,
-                      color: "white",
-                  }}
-              >
-                  Scanned Result: {scannedResult}
-              </p>
-          )}
-        {(!qrOn && !scannedResult)? <IonButton expand='full' onClick={() => startScanner()}>Start</IonButton> : null}
-        {qrOn? <IonButton expand="full" onClick={() => {scanner?.current?.stop(); setQrOn(false)}}>Stop</IonButton> : null}
         </>
-
-        
+      ) : (
+        <>
+        {scannedResult && (addToSessionMode || newSessionMode) ? (<>
+          {addToSessionMode ? (<>
+            <IonItem detail key="some1string2and3some4integers5" onClick={() => {console.log("Test complete")}}>Session 1</IonItem>
+          </>) : (<>
+            
+          </>)}
+      </>) : (<>
+        <IonButton expand="full" color='primary' onClick={() => {setAddToSessionMode(true)}}>Add Table to Session</IonButton>
+        <IonButton expand="full" color='primary' onClick={() => {setNewSessionMode(true)}}>Create new Session with Table</IonButton> 
+      </>)}
+        </>
+      )}
+      
+    </>
   );
 };
 
--- frontend/src/components/ScannerModal.tsx
+++ frontend/src/components/ScannerModal.tsx
@@ -7,7 +7,7 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import QrReader from "./QRScanner";
 
 interface ScannerModalProps {
@@ -19,18 +19,21 @@ const ScannerModal: React.FC<ScannerModalProps> = ({
   isScannerOpen,
   setIsScannerOpen,
 }) => {
+
+  const [modalTitle, setModalTitle] = useState<string>("Scan QR Code");
+  
   return (
     <IonModal isOpen={isScannerOpen}>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Scan QR-Code</IonTitle>
+          <IonTitle>{modalTitle}</IonTitle>
           <IonButtons slot="end">
             <IonButton color='primary' onClick={() => setIsScannerOpen(false)}>Close</IonButton>
           </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <QrReader/>
+        <QrReader setModalIsOpen={setIsScannerOpen} setModelTitle={setModalTitle}/>
       </IonContent>
     </IonModal>
   );
