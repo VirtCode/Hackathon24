0f161f608c3941f79d38e4ff18cf0ab541428433
Arthur Hennig <ahennig@student-net-hg-0777.intern.ethz.ch>
2024-10-12 21:04:23 +0200

    fix group add group

--- frontend/src/pages/Groups.tsx
+++ frontend/src/pages/Groups.tsx
@@ -13,7 +13,6 @@ import { add } from "ionicons/icons";
 import AddGroup from "../components/AddGroup";
 import { useState, useEffect } from "react";
 import { getAllGroupsOfUser } from "../api/group";
-import { useHistory } from "react-router-dom";
 import { Group } from "../api/group";
 
 interface GroupsProps {
@@ -23,21 +22,6 @@ interface GroupsProps {
 
 const Groups: React.FC<GroupsProps> = ({ groups, setGroups }) => {
   const [showAddGroupModal, setShowAddGroupModal] = useState(false);
-  // const history = useHistory();
-
-  // useEffect(() => {
-  //   history.listen((location) => {
-  //     getAllGroupsOfUser(setGroups);
-  //   });
-  // }, [history]);
-
-  // useEffect(() => {
-  //   getAllGroupsOfUser(setGroups);
-  // }, [showAddGroupModal]);
-
-  // useIonViewWillEnter(() => {
-  //   getAllGroupsOfUser(setGroups);
-  // });
 
   return (
     <IonPage>
@@ -46,10 +30,7 @@ const Groups: React.FC<GroupsProps> = ({ groups, setGroups }) => {
         <GroupList groups={groups}></GroupList>
 
         <IonFab slot="fixed" vertical="bottom" horizontal="center">
-          <IonFabButton
-            id="fabButton"
-            onClick={() => setShowAddGroupModal(true)}
-          >
+          <IonFabButton onClick={() => setShowAddGroupModal(true)}>
             <IonIcon icon={add}></IonIcon>
           </IonFabButton>
         </IonFab>
