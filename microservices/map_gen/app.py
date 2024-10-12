7d1bf93e09c5f7aa5d2c3796b4db6beff64445f9
Raiqou <56829016+Raiqou@users.noreply.github.com>
2024-10-12 15:27:54 +0200

    map_gen: added picking map by uuid

--- microservices/map_gen/app.py
+++ microservices/map_gen/app.py
@@ -1,12 +1,16 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import os
-
 import maps
 
-#import svgutils.transform as st
+MENSA_MAPPING = {
+    "Polysnack" : "62a02795-65cc-4b01-9172-7321362cbef4",
+    "Archimedes" : "63adc875-938c-41f0-acd2-31903e90b3d2",
+    "Polymensa" : "6d930285-affe-47a3-af8f-2e04df87af38",
+    "Clausiusbar" : "c33ab862-640a-4364-9c01-fffb40d67ae0",
+    "UZH Zentrum" : "987fca43-1288-41c8-b8cd-34d5624ee0c3"
+}
 
-myMap = maps.Map("resources/polysnack.json")
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -14,29 +18,11 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/render', methods=['POST'])
 def getRender():
     data = request.get_json()
-    print(f'MensaID: {data["id"]}')
-    #myMap = maps.addTables(maps.MAPS['polysnack'], data)
-    return maps.addTables(maps.MAPS['polysnack'], data).svgMap.as_str()
+    file_path = f'resources/{data["id"]}.json'
+    mensaMap = maps.Map(file_path)
+    return maps.addTables(mensaMap, data).svgMap.as_str()
 
 if __name__ == '__main__':
-
-    maps.MAPS['polysnack'] = maps.Map("resources/polysnack.json")
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 4444)))
-
-
-    """ 
-    ## TODO: json will be passed via flask
-    with open('data.json', 'r') as file:
-        data = json.load(file)
-
-    print(f'MensaID: {data["id"]}')
-    ## TODO: Check for Mensa Map
-    ## TODO: myMap = maps[data['id']]
-
-    maps.addTables(maps.MAPS['polysnack'], data)
-
-    my_svg2 = st.fromstring(myMap.svgMap.as_str())
-    my_svg2.save('resources/my_svg.svg')
-    """
 
 
